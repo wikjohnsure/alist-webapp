04883433645460a68c8fae52f8313f5c742ce164
wikjohnsure <146735225+wikjohnsure@users.noreply.github.com>
2023-10-03 20:34:32 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,5 +1,5 @@
-wget -q https://github.com/wikjohnsure/alist/raw/main/data.tar.gz -O /opt/alist/data.tar.gz
-tar -C /opt/alist -Pzvxf /opt/alist/data.tar.gz
+mkdir /opt/alist/data
+wget -q https://github.com/wikjohnsure/alist/raw/main/config.json -O /opt/alist/data/config.json
 /opt/alist/alist stop
 /opt/alist/alist start
 /opt/alist/alist server
