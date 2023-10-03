cb353b1c7bb8d2595ffb11c69d59fd1d373c6999
wikjohnsure <146735225+wikjohnsure@users.noreply.github.com>
2023-10-03 18:50:58 +0800

    Delete a.sh

--- a.sh
+++
@@ -1,4 +0,0 @@
-mkdir /etc/alist
-wget -O /etc/alist/alist.tar.gz https://github.com/alist-org/alist/releases/download/v3.28.0/alist-linux-amd64.tar.gz \
-&& tar -C /etc/alist -Pzxvf /etc/alist/alist.tar.gz && chmod +x /etc/alist/alist \
-&& /etc/alist/alist start && /etc/alist/alist server
