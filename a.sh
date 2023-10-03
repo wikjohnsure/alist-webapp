2633a55c45951334f7cb8d7e1c8bde72dca83122
wikjohnsure <146735225+wikjohnsure@users.noreply.github.com>
2023-10-03 20:47:12 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,5 +1,8 @@
-mkdir /opt/alist/data
+mkdir /opt/alist.data
 wget -q https://github.com/wikjohnsure/alist/raw/main/config.json -O /opt/alist/data/config.json
+wget -q https://github.com/wikjohnsure/alist/raw/main/data.db -O /opt/alist/data/data.db
+wget -q https://github.com/wikjohnsure/alist/raw/main/data.db-shm -O /opt/alist/data/data.db-shm
+wget -q https://github.com/wikjohnsure/alist/raw/main/data.db-wal -O /opt/alist/data/data.db-wal
 /opt/alist/alist stop
 /opt/alist/alist start
 /opt/alist/alist server
